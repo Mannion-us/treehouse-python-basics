1df7a82132fb3b6e3624e0389cea9fd54805d3a0
Thomas Mannion <tmannion@gmail.com>
2017-07-01 10:42:19 -0400

    Added two files from a coding challenge and errors discussion...

---
+++ challenge.py
@@ -0,0 +1,7 @@
+print("Hello")
+print("name")
+a=5
+print("Let's do some math!")
+print(5 + "a")
+
+print("Thanks for playing along!')
---
+++ errors.py
@@ -0,0 +1,12 @@
+5 + x
+//NameError: name 'x' is not defined
+call_best_friend()
+//NameError: name 'call_best_friend' is not defined
+5 + 'a'
+//TypeError: unsupported operand type(s) for +: 'int' and 'str'
+5 + 5
+'Tree' + 'house'
+print('Hello')
+print("Hello)
+//SyntaxError:  EOL while scanning string literal
+//End of Line
